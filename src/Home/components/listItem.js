3176737c43ce8a837d5868ed28040b7aa080c62d
lhelminiak <l.helminiak@yahoo.com>
2018-02-22 17:02:05 -0600

    Commiting current state of list items

--- src/Home/components/listItem.js
+++ src/Home/components/listItem.js
@@ -17,23 +17,47 @@ export default class ListItem extends React.Component {
     render() {
         return (
             <View style={styles.listItem}>
+
                 <View style={styles.itemIndicatorContainer}>
-                    <Text>{this.props.title}</Text>
+                    <View style={[styles.indicator, styles.profitTradeIndicator]}>
+
+                    </View>
 
                 </View>
+
+
 
                 <View style={styles.itemInfoContainer}>
+                    <View style={styles.itemInfoTop}>
+                        <Text style={styles.tickerSymbol}>AAPL</Text>
+                        <Text style={styles.tradeDate}> Today 12:00 PM</Text>
+                    </View>
+
+                    <View style={styles.itemInfoBottom}>
+                        <View style={styles.tradeNotePreviewContainer}>
+                            <Text style={styles.tradeNotePreview} numberOfLines={1} ellipsizeMode="tail">Waiting for trend to reverse before exiting</Text>
+                        </View>
+
+                        <View style={styles.tradePositionContainer}>
+                            <Text style={styles.tradePosition}>Long</Text>
+                        </View>
+
+
+                    </View>
 
                 </View>
+
 
                 <View style={styles.itemButtonContainer}>
 
                 </View>
 
-
-
-
             </View>
+
+
+
+
+
         );
 
     }
@@ -47,27 +71,135 @@ const styles = StyleSheet.create({
         flexWrap: 'nowrap',
         justifyContent: 'flex-start',
         alignItems: 'stretch',
-        alignContent: 'flex-start'
+        alignContent: 'flex-start',
+        height: 60,
     },
 
     itemIndicatorContainer: {
-        flex: 1,
+        flexGrow: 0,
+        flexShrink: 0,
+        flexBasis: '20%',
         alignSelf: 'auto',
-        backgroundColor: 'white',
+        backgroundColor: '#f5f5f5',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: '2%',
+        paddingBottom: '2%',
 
     },
+
+    indicator: {
+        height: '100%',
+        width: '60%',
+        borderRadius: 50,
+        paddingTop: '2%',
+        paddingBottom: '2%',
+
+    },
+    openTradeIndicator: {
+        backgroundColor: '#cccccc',
+    },
+    profitTradeIndicator: {
+        backgroundColor: '#21CE99',
+    },
+    lossTradeIndicator: {
+        backgroundColor: '#F2522E',
+    },
+    evenTradeIndicator: {
+        backgroundColor: '#cccccc',
+    },
+
+
 
     itemInfoContainer: {
         flex: 1,
         alignSelf: 'auto',
-        backgroundColor: 'red',
+        backgroundColor: '#f5f5f5',
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        alignContent: 'flex-start',
+
+
     },
 
-    itemButtonContainer: {
+    itemInfoTop: {
         flex: 1,
         alignSelf: 'auto',
-        backgroundColor: 'green'
-    }
+        position: 'relative',
+
+
+    },
+
+    tickerSymbol:{
+        fontSize: 20,
+        fontWeight: '700',
+        position: 'absolute',
+        left: 1,
+        top: 2,
+    },
+
+    tradeDate: {
+        fontSize: 12,
+        position: 'absolute',
+        right: 3,
+        top: 2,
+        color: '#9b9b9b',
+    },
+
+    itemInfoBottom:{
+        flex: 1,
+        alignSelf: 'auto',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        alignContent: 'flex-start',
+
+    },
+
+    tradeNotePreviewContainer:{
+        flexGrow: 0,
+        flexShrink: 0,
+        flexBasis: '75%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    tradeNotePreview:{
+        color: '#9b9b9b',
+    },
+
+    tradePositionContainer:{
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    tradePosition:{
+        color: '#9b9b9b',
+    },
+
+
+
+    itemButtonContainer: {
+        flexGrow: 0,
+        flexShrink: 0,
+        flexBasis: '5%',
+        alignSelf: 'auto',
+        backgroundColor: '#f5f5f5',
+
+    },
 
 
 
